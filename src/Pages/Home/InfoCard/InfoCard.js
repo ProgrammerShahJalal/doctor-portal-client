a08e37fb217adcfa5c22b0f9b94418b0d7923592
Md Shah Jalal <bestwebdesignerr@gmail.com>
2021-11-16 09:17:21 +0600

    animation add

--- src/Pages/Home/InfoCard/InfoCard.js
+++ src/Pages/Home/InfoCard/InfoCard.js
@@ -1,14 +1,21 @@
 import { Container, Grid, Paper, Typography } from '@mui/material';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { infoData } from '../../../data/data';
+import AOS from 'aos';
+import 'aos/dist/aos.css'
 
 const InfoCard = () => {
+    useEffect(() => {
+        AOS.init();
+    }, [])
     return (
         <Container>
             <Grid container spacing={3}>
                 {
                     infoData.map(({ Icon, title, description, background }) => <Grid key={title} item xs={12} sm={12} md={4} lg={4}>
-                        <Paper variant="outlined" sx={{ display: 'flex', justifyContent: 'space-between', background: background, color: '#fff', p: 3 }}>
+                        <Paper data-aos="fade-down"
+                            data-aos-easing="linear"
+                            data-aos-duration="1500" variant="outlined" sx={{ display: 'flex', justifyContent: 'space-between', background: background, color: '#fff', p: 3 }}>
                             <Icon sx={{ fontSize: 60 }} />
                             <div>
                                 <Typography variant="body1">{title}</Typography>
